8111afebf20a09b0768697aae24af185685d9b21
fredomain <jmao.mechatronics@gmail.com>
2025-03-30 19:21:47 +0200

    New code version added with more SDL3 function examples

--- SDL3_Testing/Source/main.cpp
+++ SDL3_Testing/Source/main.cpp
@@ -24,11 +24,22 @@ void Close();		// Frees media and shuts down SDL
 
 /********** GLOBALS **********/
 SDL_Window* gWindow = NULL;				// The window we'll be rendering to
-SDL_Surface* gScreenSurface = NULL;		// The surface contained by the window
+SDL_Renderer* gRenderer = NULL;			//The window renderer
+
+
 SDL_Surface* gHelloWorld = NULL;		// The image we will load and show on the screen
 SDL_Surface* character = NULL;
-SDL_Rect character_ori;
-SDL_Rect character_dest;
+SDL_Texture* gHelloWorld_t = NULL;		// The image we will load and show on the screen
+SDL_Texture* character_t = NULL;
+
+SDL_FRect character_ori;
+SDL_FRect character_dest;
+
+
+//Main loop flag
+bool quit = false;
+//Event handler
+SDL_Event event;
 /*****************************/
 
 int main(int argc, char* args[]){
@@ -42,22 +53,60 @@ int main(int argc, char* args[]){
 			SDL_Log("Failed to load media!\n");
 		}
 		else{
-			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			character_dest.x = 300.0f;
+			character_dest.y = 120.0f;
+			character_dest.w = character_t->w * 0.5f;
+			character_dest.h = character_t->h * 0.5f;
 
+			//While application is running
+			while (!quit)
+			{
 
-			character_ori.x = 20;
-			character_ori.y = 40;
-			character_ori.w = 180;
-			character_ori.h = 120;
-			character_dest.x = 300;
-			character_dest.y = 120;
+				if (SDL_PollEvent(&event) != 0)
+				{
+					//User requests quit
+					if (event.type == SDL_EVENT_QUIT)
+					{
+						quit = true;
+					}
+					else if (event.type == SDL_EVENT_KEY_DOWN) {
+						//Select surfaces based on key press
+						switch (event.key.key)
+						{
+						case SDLK_UP:
+							character_dest.y -= 10.0f;
+							break;
 
-			SDL_BlitSurface(character, &character_ori, gScreenSurface, &character_dest);
+						case SDLK_DOWN:
+							character_dest.y += 10.0f;
+							break;
 
-			SDL_UpdateWindowSurface(gWindow);	//Update the surface
+						case SDLK_LEFT:
+							character_dest.x -= 10.0f;
+							break;
 
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_EVENT_QUIT) quit = true; } }
+						case SDLK_RIGHT:
+							character_dest.x += 10.0f;
+							break;
+						}
+					}
+				}
+
+				// Clear screen
+				SDL_RenderClear(gRenderer);
+
+				// Render texture to screen
+				SDL_RenderTexture(gRenderer, gHelloWorld_t, NULL, NULL);
+				SDL_RenderTexture(gRenderer, character_t, NULL, &character_dest);
+
+				// Update screen
+				SDL_RenderPresent(gRenderer);
+
+				
+
+				// Hack to get window to stay up
+				//SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_EVENT_QUIT) quit = true; } }
+			}
 		}
 	}
 
@@ -74,16 +123,18 @@ bool Init(){
 	if (!SDL_Init(SDL_INIT_VIDEO)){
 		SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 		success = false;
-	}
-	else{
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Testing", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-		if (gWindow == NULL){ 
-			SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+	}else
+	{
+		//Create window and renderer
+		if (!SDL_CreateWindowAndRenderer("SDL Tutorial", SCREEN_WIDTH, SCREEN_HEIGHT, 0, &gWindow, &gRenderer))
+		{
+			SDL_Log("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 			success = false;
 		}
-		else{
-			gScreenSurface = SDL_GetWindowSurface(gWindow);	//Get window surface
+		else
+		{
+			//Initialize renderer color
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		}
 	}
 
@@ -99,6 +150,16 @@ bool LoadMedia(){
 	if (gHelloWorld == NULL){
 		SDL_Log("Unable to load image %s! SDL Error: %s\n", "background.png", SDL_GetError());
 		success = false;
+	}else{
+		//Create texture from surface pixels
+		gHelloWorld_t = SDL_CreateTextureFromSurface(gRenderer, gHelloWorld);
+		if (gHelloWorld_t == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", "Content / Images / background.png", SDL_GetError());
+		}
+
+		//Get rid of old loaded surface
+		SDL_DestroySurface(gHelloWorld);
 	}
 
 	// Load character
@@ -107,14 +168,25 @@ bool LoadMedia(){
 		SDL_Log("Unable to load image %s! SDL Error: %s\n", "character.png", SDL_GetError());
 		success = false;
 	}
+	else {
+		//Create texture from surface pixels
+		character_t = SDL_CreateTextureFromSurface(gRenderer, character);
+		if (character_t == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", "character.png", SDL_GetError());
+		}
+
+		//Get rid of old loaded surface
+		SDL_DestroySurface(character);
+	}
 
 	return success;
 }
 
 void Close(){
 	// Deallocate surfaces
-	SDL_DestroySurface(gHelloWorld);
-	SDL_DestroySurface(gScreenSurface);
+	SDL_DestroyTexture(gHelloWorld_t);
+	SDL_DestroyTexture(character_t);
 
 	// Destroy window
 	SDL_DestroyWindow(gWindow);
