26dc40d9725c40aeac6329ea5e510555d69f4680
Juan Manuel Amador <jmao.mechatronics@gmail.com>
2025-05-30 10:50:59 +0200

    Added CreatineEngineCore project

---
+++ CreatineEngineCore/Source/Public/Actor.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef ACTOR_H
+
+//Using SDL
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+//#include <SDL_mixer.h>
+
+#include <print>
+
+namespace CE {
+	class Actor
+	{
+	};
+}
+
+#endif // !ACTOR_H
