b78fcf7fe9d26a6b31df48b08de4744f047cd610
Gonzalo <mori.642@gmail.com>
2025-06-05 08:24:57 +0200

    Creation of constructors, getter, setter & loadfromfile placeholder

--- CreatineEngineCore/Source/Public/Image.h
+++ CreatineEngineCore/Source/Public/Image.h
@@ -8,6 +8,32 @@
 namespace CE {
 	class Image : public GPU_Surface
 	{
+	public:
+		Image();																//Constructor
+		~Image();																//Destructor
+
+		bool loadFromFile(const std::string path, SDL_Renderer* gRenderer);		//Load file and convert to Texture
+
+		/* SETTERS AND GETTERS */
+		int const xPos();
+		int const yPos();
+		int const width();
+		int const height();
+
+		void xPos(int xPos);
+		void yPos(int xPos);
+		void width(int width);
+		void height(int height);
+
+	private:
+		SDL_Texture* imageTexture;												//SDL Texture		
+
+		int xPos_;																//Texture X position
+		int yPos_;																//Texture Y position
+
+		int width_;																//Texture width
+		int height_;															//Texture height
+
 	};
 
 }
