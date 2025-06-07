0e66f79c47e9d8447a44899d9b9ccc4a65ea8ebd
Abathar <andresdelgadodominguez@gmail.com>
2025-06-07 19:34:30 +0200

    added-Text-Class

---
+++ CreatineEngineCore/Text.h
@@ -0,0 +1,14 @@
+
+//ifndef to avoid errors on multiple declarations of this header
+#ifndef TEXT_H
+#define TEXT_H
+
+#include "Image.h" //Parent header
+
+namespace CE {
+	class Text : public Image
+	{
+	};
+}
+
+#endif // !TEXT_H
