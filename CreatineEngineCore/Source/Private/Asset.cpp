30b8dbfa46113d66b6d38db49c761e006dd031bc
Gonzalo <mori.642@gmail.com>
2025-07-01 07:02:29 +0200

    First documentation/comment additions

--- CreatineEngineCore/Source/Private/Asset.cpp
+++ CreatineEngineCore/Source/Private/Asset.cpp
@@ -2,20 +2,36 @@
 #include <memory>
 
 namespace CE {
-
+    /**
+     * @brief Asset constructor
+     * @param path : path to the Asset file (image, sound, text font...)
+     */
     Asset::Asset(std::string path)
         : path(std::move(path)) {
     }
 
+    /**
+     * @brief 
+     * @param newPath 
+     * @return 
+     */
     bool Asset::load(const std::string& newPath) {
         setPath(newPath);
         return load();
     }
 
+    /**
+     * @brief path Getter
+     * @return Asset path as string
+     */
     std::string Asset::getPath() const {
         return path;
     }
 
+    /**
+     * @brief path Setter
+     * @param newPath string value to replace the current "path"
+     */
     void Asset::setPath(const std::string& newPath) {
         path = newPath;
     }
